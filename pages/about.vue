57c4ff65f4abda3b9b5e698d406ef24631b6739e
Vasily Kuzin <exer7um@gmail.com>
2021-05-21 13:50:35 +0000

    Обо мне #15

---
+++ pages/about.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <AboutMe class="mt-4" />
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Обо мне | exer7um.github.io',
+    }
+  },
+}
+</script>

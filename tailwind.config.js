75330debba051cf9f6dff6ac0467a38b7504e949
Vasily Kuzin <exer7um@gmail.com>
2021-05-29 06:11:50 +0000

    Safelist для PurgeCSS, fix #31

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 module.exports = {
-  purge: [
-    './components/**/*.{vue,js}',
-    './layouts/**/*.vue',
-    './pages/**/*.vue',
-    './plugins/**/*.{js,ts}',
-    './nuxt.config.{js,ts}',
-  ],
+  purge: {
+    content: [
+      './components/**/*.{vue,js}',
+      './layouts/**/*.vue',
+      './pages/**/*.vue',
+      './plugins/**/*.{js,ts}',
+      './nuxt.config.{js,ts}',
+    ],
+    options: {
+      safelist: { standard: [/^bg-/] },
+    },
+  },
   darkMode: 'media',
   theme: {
     container: {

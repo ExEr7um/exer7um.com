b9555ab6d8f6d026a3160d64c99463f9490e7d6c
Vasily Kuzin <exer7um@gmail.com>
2022-01-29 16:33:16 +0300

    Изменил анимацию загрузки #188

--- components/projects/ProjectLoading.vue
+++ components/projects/ProjectLoading.vue
@@ -1,88 +1,30 @@
 <template>
   <div>
-    <svg
-      role="img"
-      width="290"
-      height="352"
-      aria-labelledby="loading-aria"
-      viewBox="0 0 290 352"
-      preserveAspectRatio="none"
-      class="w-full"
+    <div
+      class="flex flex-col overflow-hidden rounded-md border dark:border-gray-600"
     >
-      <title id="loading-aria">Loading...</title>
-      <rect
-        x="0"
-        y="0"
-        width="100%"
-        height="100%"
-        clip-path="url(#clip-path)"
-        style="fill: url('#fill')"
-      ></rect>
-      <defs>
-        <clipPath id="clip-path">
-          <rect x="0" y="0" rx="10" ry="10" width="290" height="159" />
-          <rect x="0" y="180" rx="6" ry="6" width="220" height="28" />
-          <rect x="0" y="230" rx="4" ry="4" width="270" height="12" />
-          <rect x="0" y="254" rx="4" ry="4" width="220" height="12" />
-          <rect x="0" y="310" rx="4" ry="4" width="60" height="16" />
-          <rect x="150" y="310" rx="4" ry="4" width="140" height="16" />
-        </clipPath>
-        <linearGradient id="fill">
-          <stop id="stop-1" offset="0.599964" stop-opacity="1">
-            <animate
-              attributeName="offset"
-              values="-2; -2; 1"
-              keyTimes="0; 0.25; 1"
-              dur="2s"
-              repeatCount="indefinite"
-            ></animate>
-          </stop>
-          <stop id="stop-2" offset="1.59996" stop-opacity="1">
-            <animate
-              attributeName="offset"
-              values="-1; -1; 2"
-              keyTimes="0; 0.25; 1"
-              dur="2s"
-              repeatCount="indefinite"
-            ></animate>
-          </stop>
-          <stop id="stop-3" offset="2.59996" stop-opacity="1">
-            <animate
-              attributeName="offset"
-              values="0; 0; 3"
-              keyTimes="0; 0.25; 1"
-              dur="2s"
-              repeatCount="indefinite"
-            ></animate>
-          </stop>
-        </linearGradient>
-      </defs>
-    </svg>
+      <div class="flex h-40 min-h-[10rem] animate-pulse bg-gray-200 p-8"></div>
+      <div class="flex h-full flex-col justify-between p-5">
+        <div>
+          <div
+            class="mb-7 h-7 w-3/4 animate-pulse rounded-md bg-gray-200"
+          ></div>
+          <div
+            class="my-4 h-4 w-full animate-pulse rounded-md bg-gray-200"
+          ></div>
+          <div
+            class="my-4 h-4 w-1/2 animate-pulse rounded-md bg-gray-200"
+          ></div>
+        </div>
+        <div class="mt-6 flex items-center justify-between">
+          <div class="h-6 w-12 animate-pulse rounded-md bg-gray-200"></div>
+          <div class="h-6 w-28 animate-pulse rounded-md bg-gray-200"></div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {}
 </script>
-
-<style>
-#stop-1,
-#stop-3 {
-  stop-color: #f3f4f6;
-}
-
-#stop-2 {
-  stop-color: #e5e7eb;
-}
-
-@media (prefers-color-scheme: dark) {
-  #stop-1,
-  #stop-3 {
-    stop-color: #374151;
-  }
-
-  #stop-2 {
-    stop-color: #4b5563;
-  }
-}
-</style>

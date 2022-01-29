27acc515be81068cfd3620f54428aaef15df42b3
Vasily Kuzin <exer7um@gmail.com>
2022-01-29 21:40:45 +0300

    Адаптация анимации загрузки под темную тему (#202)

--- components/projects/ProjectLoading.vue
+++ components/projects/ProjectLoading.vue
@@ -3,22 +3,28 @@
     <div
       class="flex flex-col overflow-hidden rounded-md border dark:border-gray-600"
     >
-      <div class="flex h-40 min-h-[10rem] animate-pulse bg-gray-200 p-8"></div>
+      <div
+        class="flex h-40 min-h-[10rem] animate-pulse rounded-t-md bg-gray-200 p-8 dark:bg-gray-600"
+      ></div>
       <div class="flex h-full flex-col justify-between p-5">
         <div>
           <div
-            class="mb-7 h-7 w-3/4 animate-pulse rounded-md bg-gray-200"
+            class="mb-7 h-7 w-3/4 animate-pulse rounded-md bg-gray-200 dark:bg-gray-600"
           ></div>
           <div
-            class="my-4 h-4 w-full animate-pulse rounded-md bg-gray-200"
+            class="my-4 h-4 w-full animate-pulse rounded-md bg-gray-200 dark:bg-gray-600"
           ></div>
           <div
-            class="my-4 h-4 w-1/2 animate-pulse rounded-md bg-gray-200"
+            class="my-4 h-4 w-1/2 animate-pulse rounded-md bg-gray-200 dark:bg-gray-600"
           ></div>
         </div>
         <div class="mt-6 flex items-center justify-between">
-          <div class="h-6 w-12 animate-pulse rounded-md bg-gray-200"></div>
-          <div class="h-6 w-28 animate-pulse rounded-md bg-gray-200"></div>
+          <div
+            class="h-6 w-12 animate-pulse rounded-md bg-gray-200 dark:bg-gray-600"
+          ></div>
+          <div
+            class="h-6 w-28 animate-pulse rounded-md bg-gray-200 dark:bg-gray-600"
+          ></div>
         </div>
       </div>
     </div>

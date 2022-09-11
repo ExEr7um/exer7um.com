bfb0a3a146d51ff03a68bcea556b660219ebdb9a
Vasily Kuzin <exer7um@gmail.com>
2022-09-11 22:27:37 +0300

    Переключатель локализации #377

---
+++ components/LocaleSwitch.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="mr-6 flex items-center divide-x divide-zinc-500">
+    <NuxtLink
+      v-for="{ code } in $i18n.locales"
+      :key="code"
+      class="cursor-pointer px-2 uppercase transition-colors"
+      :class="[
+        $i18n.locale === code
+          ? 'text-zinc-900 dark:text-zinc-100'
+          : 'text-zinc-500 hover:text-zinc-900 dark:hover:text-zinc-100',
+      ]"
+      :to="switchLocalePath(code)"
+    >
+      {{ code }}
+    </NuxtLink>
+  </div>
+</template>

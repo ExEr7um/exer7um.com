35dcfc2eae84550b12ceed4676b6c448cebfe618
Vasily Kuzin <exer7um@gmail.com>
2024-03-15 21:08:01 +0300

    Настройка `i18nHead`

--- app.vue
+++ app.vue
@@ -1,4 +1,17 @@
 <script setup lang="ts">
+const i18nHead = useLocaleHead({
+  addDirAttribute: true,
+  addSeoAttributes: true,
+})
+
+useHeadSafe({
+  htmlAttrs: {
+    lang: i18nHead.value.htmlAttrs!.lang,
+  },
+  link: [...(i18nHead.value.link || [])],
+  meta: [...(i18nHead.value.meta || [])],
+})
+
 useSeoMeta({
   titleTemplate: `%s | ExEr7um`,
 })

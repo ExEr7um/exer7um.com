adad9c433a9e0c7764eb285c05a338ff0d6ca659
Vasily Kuzin <exer7um@gmail.com>
2024-03-15 21:11:47 +0300

    feat: Настройка проекта (#414)
    - Конфигурация Nuxt
    - Настройка Tailwind CSS
    - Настройка шрифтов
    - Настройка VS Code
    - Настройка локализации
    
    ### Связанная задача
    Fixes EXR7-101.

--- app.vue
+++ app.vue
@@ -1,5 +1,25 @@
+<script setup lang="ts">
+const i18nHead = useLocaleHead({
+  addDirAttribute: true,
+  addSeoAttributes: true,
+})
+
+useHeadSafe({
+  htmlAttrs: {
+    lang: i18nHead.value.htmlAttrs!.lang,
+  },
+  link: [...(i18nHead.value.link || [])],
+  meta: [...(i18nHead.value.meta || [])],
+})
+
+useSeoMeta({
+  titleTemplate: `%s | ExEr7um`,
+})
+</script>
+
 <template>
   <div>
-    <NuxtWelcome />
+    <NuxtLoadingIndicator color="#2563EB" />
+    <NuxtLayout />
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,77 @@
 export default defineNuxtConfig({
+  appConfig: {
+    nuxtIcon: {
+      size: "24px",
+    },
+  },
   devtools: {
     enabled: true,
   },
+  experimental: {
+    componentIslands: {
+      selectiveClient: true,
+    },
+    cookieStore: true,
+    headNext: true,
+    typedPages: true,
+  },
+  fonts: {
+    experimental: {
+      addPreloadLinks: true,
+    },
+    families: [
+      {
+        name: "Gill Sans",
+        provider: "local",
+        weights: [400, 700],
+      },
+    ],
+  },
+  i18n: {
+    defaultLocale: "ru",
+    langDir: "assets/lang/",
+    lazy: true,
+    locales: [
+      {
+        code: "ru",
+        file: "ru.json",
+        iso: "ru-RU",
+        name: "Русский",
+      },
+      {
+        code: "en",
+        file: "en.json",
+        iso: "en-US",
+        name: "English",
+      },
+    ],
+    trailingSlash: true,
+  },
   modules: [
+    "@formkit/auto-animate/nuxt",
+    "@nuxt/fonts",
+    "@nuxt/image",
+    "@nuxtjs/i18n",
+    "@nuxtjs/tailwindcss",
+    "@vueuse/nuxt",
+    "nuxt-headlessui",
+    "nuxt-icon",
     // Если запускается билд, то @nuxt/test-utils не нужен
     process.env.NODE_ENV === "production"
       ? () => undefined
       : "@nuxt/test-utils/module",
   ],
+  postcss: {
+    plugins: {
+      "tailwindcss/nesting": "postcss-nesting",
+    },
+  },
+  router: {
+    options: {
+      scrollBehaviorType: "smooth",
+    },
+  },
+  tailwindcss: {
+    viewer: false,
+  },
 })

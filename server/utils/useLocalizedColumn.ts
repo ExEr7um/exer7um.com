bf0be43109b1964902dd201f7e2d77561a98a499
Vasily Kuzin <exer7um@gmail.com>
2024-03-19 22:35:13 +0300

    Функция для получения локализированной колонки

---
+++ server/utils/useLocalizedColumn.ts
@@ -0,0 +1,18 @@
+import type { H3Event } from "h3"
+
+import { getHeaderLocale } from "@intlify/h3"
+
+/**
+ * Функция для получения локализированного названия колонки.
+ *
+ * @param column - название колонки.
+ * @param event - H3 ивент.
+ * @returns локализированное название колонки
+ */
+export default function <TReturn>(column: string, event: H3Event): TReturn {
+  const locale = getHeaderLocale(event, {
+    lang: "ru",
+  })
+
+  return (column + locale.baseName.toUpperCase()) as TReturn
+}

01046969bea273d2f2501c37d651edae26260286
Vasily Kuzin <exer7um@gmail.com>
2024-03-19 18:07:23 +0300

    feat(api): Сущность «Технология» (#419)
    Добавлена сущность используемой в проекте технологии.
    
    ### Связанная задача
    - Fixes EXR7-97

--- server/utils/drizzle.ts
+++ server/utils/drizzle.ts
@@ -1,12 +1,20 @@
 import { drizzle } from "drizzle-orm/d1"
 
+import * as tagsSchema from "../database/schema/tag"
+
+export const tables = {
+  ...tagsSchema,
+}
+
 /**
  * Функция для обращения к Drizzle ORM.
  *
  * @returns методы для работы с Drizzle ORM.
  */
 export function useDrizzle() {
-  return drizzle(hubDatabase())
+  return drizzle(hubDatabase(), {
+    schema: tables,
+  })
 }
 
 // Экспортируем методы Drizzle для автоимпорта

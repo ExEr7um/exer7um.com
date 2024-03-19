ffde2e4d2b2776a6b1f06f70025c36c8adcd3dda
Vasily Kuzin <exer7um@gmail.com>
2024-03-19 18:43:16 +0300

    Добавление поля `createdAt`

--- server/database/schema/personalProject.ts
+++ server/database/schema/personalProject.ts
@@ -4,6 +4,7 @@ import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core"
 import { tagsToPersonalProjects } from "./tagsToPersonalProjects"
 
 export const personalProjects = sqliteTable("personal_projects", {
+  createdAt: integer("created_at", { mode: "timestamp" }).notNull(),
   description: text("description").notNull(),
   github: text("github"),
   icon: text("icon", { length: 1 }).notNull(),

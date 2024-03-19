a687e7e0cddad9ffa7ea3202859e04b04332eb84
Vasily Kuzin <exer7um@gmail.com>
2024-03-19 18:38:33 +0300

    Сущность «Персональный проект»

---
+++ server/database/schema/personalProject.ts
@@ -0,0 +1,21 @@
+import { relations } from "drizzle-orm"
+import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core"
+
+import { tagsToPersonalProjects } from "./tagsToPersonalProjects"
+
+export const personalProjects = sqliteTable("personal_projects", {
+  description: text("description").notNull(),
+  github: text("github"),
+  icon: text("icon", { length: 1 }).notNull(),
+  id: integer("id").primaryKey({ autoIncrement: true }),
+  title: text("title").notNull(),
+})
+
+export type personalProject = typeof personalProjects.$inferSelect
+
+export const personalProjectsRelations = relations(
+  personalProjects,
+  ({ many }) => ({
+    tagsToPersonalProjects: many(tagsToPersonalProjects),
+  })
+)

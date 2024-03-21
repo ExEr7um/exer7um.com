c2afd3152876647f443e1a8bbb9abd72152e5dfe
Vasily Kuzin <exer7um@gmail.com>
2024-03-21 17:21:25 +0300

    Тесты

---
+++ tests/server/utils/useLocalizedColumn.spec.ts
@@ -0,0 +1,39 @@
+import useLocalizedColumn from "@/server/utils/useLocalizedColumn"
+import { createEvent } from "h3"
+import { IncomingMessage, ServerResponse } from "http"
+import { Socket } from "net"
+import { beforeEach, describe, expect, test } from "vitest"
+
+describe("Функция useLocalizedColumn", () => {
+  /** Название колонки */
+  const columnName = "column"
+
+  /** Запрос к серверу */
+  let request: IncomingMessage
+
+  beforeEach(() => {
+    // Создаем запрос
+    request = new IncomingMessage(new Socket())
+
+    // Устанавливаем хедер Accept-Language на EN
+    request.headers["accept-language"] = "en"
+  })
+
+  const event = () => createEvent(request, new ServerResponse(request))
+  const localizedColumn = () => useLocalizedColumn(columnName, event())
+
+  describe("Передан хедер EN", () => {
+    test("Возвращается английское название колонки", () => {
+      expect(localizedColumn()).toBe(columnName + "EN")
+    })
+  })
+
+  describe("Не передан хедер локализации", () => {
+    test("Возвращается русское название колонки", () => {
+      // Устанавливаем хедер Accept-Language на undefined
+      request.headers["accept-language"] = undefined
+
+      expect(localizedColumn()).toBe(columnName + "RU")
+    })
+  })
+})

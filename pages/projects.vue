998733b34a1f067f60397535a182d87cd557f7ea
Vasily Kuzin <exer7um@gmail.com>
2024-03-21 18:48:53 +0300

    Страница Проекты

---
+++ pages/projects.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const { t } = useI18n()
+
+useSeoMeta({
+  title: t("pages.projects"),
+  // TODO: Добавить описание страницы
+})
+</script>
+
+<template>
+  <main>
+    <h1>{{ t("pages.projects") }}</h1>
+    <PersonalProjects />
+  </main>
+</template>

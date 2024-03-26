6e5fab0e1a0cb52e195d6332460a5c7a178bde89
Vasily Kuzin <exer7um@gmail.com>
2024-03-26 13:36:06 +0300

    Социальные сети

---
+++ components/the-footer/social/TheFooterSocialCard.server.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+/** Социальная сеть */
+export interface SocialCard {
+  /** Иконка социальной сети */
+  icon: string
+  /** `URL` социальной сети */
+  url: string
+}
+
+defineProps<{
+  /** Социальная сеть */
+  socialCard: SocialCard
+}>()
+</script>
+
+<template>
+  <NuxtLink
+    class="tag flex size-12 items-center justify-center rounded-lg transition-colors hover:bg-white hover:dark:bg-neutral-700"
+    external
+    target="_blank"
+    :to="socialCard.url"
+  >
+    <Icon class="fill-current text-neutral-400" :name="socialCard.icon" />
+  </NuxtLink>
+</template>

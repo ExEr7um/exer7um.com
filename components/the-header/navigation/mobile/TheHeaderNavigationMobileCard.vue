c4505f7a8da2150ee518186f23017f5f2cf41ae4
Vasily Kuzin <exer7um@gmail.com>
2024-03-29 13:35:13 +0300

    Закрытие меню при нажатии

--- components/the-header/navigation/mobile/TheHeaderNavigationMobileCard.vue
+++ components/the-header/navigation/mobile/TheHeaderNavigationMobileCard.vue
@@ -7,8 +7,6 @@ const props = defineProps<{
 const { t } = useI18n()
 const localeRoute = useLocaleRoute()
 
-const NuxtLink = resolveComponent("NuxtLink")
-
 /** Полученный объект `route` для навигации */
 const navigationRoute = localeRoute({
   name: toRefs(props).routeName.value,
@@ -17,10 +15,9 @@ const navigationRoute = localeRoute({
 
 <template>
   <HeadlessMenuItem
-    :as="NuxtLink"
-    class="w-full rounded px-3 py-2 hover:bg-neutral-50 dark:hover:bg-neutral-800"
-    exact-active-class="hover:bg-white dark:hover:bg-neutral-900 cursor-default"
-    :to="navigationRoute"
+    as="span"
+    class="w-full cursor-pointer rounded px-3 py-2 hover:bg-neutral-50 dark:hover:bg-neutral-800"
+    @click="navigateTo(navigationRoute)"
   >
     {{ t(`pages.${routeName}`) }}
   </HeadlessMenuItem>

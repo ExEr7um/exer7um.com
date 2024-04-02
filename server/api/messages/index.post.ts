9103cdc936d75e779ceb5f00adc5d5bdaaa2379a
Vasily Kuzin <exer7um@gmail.com>
2024-04-03 01:48:14 +0300

    feat(api): Эндпоинт отправки сообщений в Telegram (#436)
    - Добавление переменных в `.env`
    - Валидация полей
    - Отправка сообщений в Telegram
    - Удаление `sequential` из тестов `TheHeaderNavigationMobile`
    
    ### Связанная задача
    Fixes EXR7-120

---
+++ server/api/messages/index.post.ts
@@ -0,0 +1,36 @@
+export interface Message {
+  /** Почта пользователя */
+  email: string
+  /** Сообщение пользователя */
+  message: string
+  /** Имя пользователя */
+  name: string
+}
+
+export default defineEventHandler(async (event) => {
+  const runtimeConfig = useRuntimeConfig()
+
+  // Получаем поля из сообщения
+  const { email, message, name } = await readBody<Message>(event)
+
+  // Если не передано хотя бы одно из полей
+  if (!email || !message || !name)
+    // Выводим ошибку
+    throw createError({
+      message: "Проверьте правильность заполненности полей",
+      status: 400,
+    })
+
+  // Отправляем запрос к боту Telegram
+  $fetch("/sendMessage", {
+    baseURL: runtimeConfig.telegramUrl,
+    body: {
+      chat_id: runtimeConfig.telegramChatId,
+      parse_mode: "HTML",
+      text: `<b>Имя:</b> ${name}\r\n<b>Почта:</b> ${email}\r\n<b>Сообщение:</b> ${message}`,
+    },
+    method: "POST",
+  })
+
+  return "OK"
+})

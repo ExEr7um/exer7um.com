775828c0b78b8b231f5979879cf9a7ea3762e02a
Vasily Kuzin <exer7um@gmail.com>
2024-04-03 00:46:17 +0300

    fix: Изменение тега `UiBlock` на `<section />` (#434)
    - Изменение тега `UiBlock` на `<section />`
    - Тестирование тега
    
    ### Связанная задача
    Fixes EXR7-125

--- components/ui/UiBlock.vue
+++ components/ui/UiBlock.vue
@@ -6,8 +6,8 @@ defineProps<{
 </script>
 
 <template>
-  <div class="flex flex-col gap-y-[3.25rem]">
+  <section class="flex flex-col gap-y-[3.25rem]">
     <h2 v-if="title">{{ title }}</h2>
     <slot />
-  </div>
+  </section>
 </template>

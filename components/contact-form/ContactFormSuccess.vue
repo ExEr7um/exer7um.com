3fc85790a0abe9c2eb254d89885a7a91566248a5
Vasily Kuzin <exer7um@gmail.com>
2024-04-09 18:53:15 +0300

    Форма обратной связи

---
+++ components/contact-form/ContactFormSuccess.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+const emit = defineEmits<{
+  close: []
+}>()
+
+const { t } = useI18n({ useScope: "local" })
+</script>
+
+<template>
+  <UiPopup
+    :description="t('description')"
+    status="success"
+    :title="t('title')"
+    @close="emit('close')"
+  >
+    <NuxtLinkLocale class="button primary mt-2 w-max" to="/">
+      {{ t("return") }}
+    </NuxtLinkLocale>
+  </UiPopup>
+</template>
+
+<i18n lang="json">
+{
+  "ru": {
+    "title": "Спасибо за ваше сообщение!",
+    "description": "Я уже начал работу над вашим запросом и обязательно свяжусь с вами в ближайшее время.",
+    "return": "Вернуться на главную"
+  },
+  "en": {
+    "title": "Thank you for your message!",
+    "description": "I have already started working on your request and will get back to you shortly.",
+    "return": "Return to homepage"
+  }
+}
+</i18n>

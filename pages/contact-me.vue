1b594f23974596c046350a08d9098d4aa3de1b0a
Vasily Kuzin <exer7um@gmail.com>
2024-04-09 18:22:31 +0300

    Страница «Написать мне»

---
+++ pages/contact-me.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const { t } = useI18n()
+
+useSeoMeta({
+  title: t("pages.contactMe"),
+  // TODO: Добавить описание страницы
+})
+</script>
+
+<template>
+  <main class="max-w-2xl">
+    <h1>{{ t("pages.contactMe") }}</h1>
+    <ContactForm />
+  </main>
+</template>

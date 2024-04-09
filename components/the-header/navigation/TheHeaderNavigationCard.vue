29cfd228dad9b5a91ba3e243afc0aa7b0afea580
Vasily Kuzin <exer7um@gmail.com>
2024-04-09 19:57:49 +0300

    fix: Исправление ссылок в `TheHeaderNavigation`
    Ссылки не были реактивными, поэтому не обновлялись при смене локализации.

--- components/the-header/navigation/TheHeaderNavigationCard.vue
+++ components/the-header/navigation/TheHeaderNavigationCard.vue
@@ -6,18 +6,17 @@ const props = defineProps<{
 
 const { t } = useI18n()
 const localeRoute = useLocaleRoute()
-
-/** Полученный объект `route` для навигации */
-const navigationRoute = localeRoute({
-  name: toRefs(props).routeName.value,
-})
 </script>
 
 <template>
   <NuxtLink
     class="relative flex justify-center leading-none transition-colors hover:text-blue-500"
     exact-active-class="cursor-default text-blue-600 after:absolute after:top-full after:mt-1.5 after:h-px after:w-6 after:bg-blue-600 hover:text-blue-600 dark:text-blue-500 after:dark:bg-blue-500 dark:hover:text-blue-500"
-    :to="navigationRoute"
+    :to="
+      localeRoute({
+        name: toRefs(props).routeName.value,
+      })
+    "
   >
     {{ t(`pages.${routeName}`) }}
   </NuxtLink>
--- components/the-header/navigation/mobile/TheHeaderNavigationMobileCard.vue
+++ components/the-header/navigation/mobile/TheHeaderNavigationMobileCard.vue
@@ -6,18 +6,19 @@ const props = defineProps<{
 
 const { t } = useI18n()
 const localeRoute = useLocaleRoute()
-
-/** Полученный объект `route` для навигации */
-const navigationRoute = localeRoute({
-  name: toRefs(props).routeName.value,
-})
 </script>
 
 <template>
   <HeadlessMenuItem
     as="span"
     class="w-full cursor-pointer rounded px-3 py-2 hover:bg-neutral-50 dark:hover:bg-neutral-800"
-    @click="navigateTo(navigationRoute)"
+    @click="
+      navigateTo(
+        localeRoute({
+          name: toRefs(props).routeName.value,
+        })
+      )
+    "
   >
     {{ t(`pages.${routeName}`) }}
   </HeadlessMenuItem>

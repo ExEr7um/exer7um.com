55ecb3211b8639433d67d670fa6c57e71a305857
Vasily Kuzin <exer7um@gmail.com>
2024-04-09 19:30:13 +0300

    Адаптив блока `TheHero`

--- components/the-hero/TheHero.vue
+++ components/the-hero/TheHero.vue
@@ -3,11 +3,13 @@ const { t } = useI18n({ useScope: "local" })
 </script>
 
 <template>
-  <section class="flex items-center justify-between gap-x-8 py-8">
+  <section
+    class="flex flex-col-reverse gap-y-12 py-8 lg:flex-row lg:items-center lg:justify-between lg:gap-x-8"
+  >
     <div class="flex max-w-[27rem] flex-col gap-y-8">
       <h1>{{ t("title") }}</h1>
       <p>{{ t("description") }}</p>
-      <div class="mt-2 flex items-center gap-x-6">
+      <div class="mt-2 flex items-center gap-x-4 sm:gap-x-6">
         <UiContactMeButton />
         <p>{{ t("or") }}</p>
         <Social compact />

5db112b8a159239207bbaa90317d66dab4bb779d
Vasily Kuzin <exer7um@gmail.com>
2024-04-17 00:34:10 +0300

    Добавление `ts-expect-error`

--- server/plugins/migrations.ts
+++ server/plugins/migrations.ts
@@ -4,6 +4,7 @@ import { migrate } from "drizzle-orm/d1/migrator"
 export default defineNitroPlugin(async () => {
   if (!import.meta.dev) return
 
+  // @ts-expect-error - при запуске typecheck не определяется тип
   onHubReady(async () => {
     await migrate(useDrizzle(), {
       migrationsFolder: "server/database/migrations",

984d2665763f149a0886a2a31027ccfeed92176a
Vasily Kuzin <exer7um@gmail.com>
2024-04-17 18:41:55 +0300

    Исправление типа дат

--- server/database/schema/workplace.ts
+++ server/database/schema/workplace.ts
@@ -1,7 +1,7 @@
 import { relations, sql } from "drizzle-orm"
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core"
 
-import { tagsToWorkplaces } from "./tagsToWorkplace"
+import { tagsToWorkplaces } from "./tagsToWorkplaces"
 
 export const workplaces = sqliteTable("workplaces", {
   createdAt: integer("created_at", { mode: "timestamp" })
@@ -9,10 +9,10 @@ export const workplaces = sqliteTable("workplaces", {
     .default(sql`(unixepoch())`),
   descriptionEN: text("description_en").notNull(),
   descriptionRU: text("description_ru").notNull(),
-  endDate: text("end_date"),
+  endDate: integer("end_date", { mode: "timestamp" }),
   id: integer("id").primaryKey({ autoIncrement: true }),
   logo: text("logo").notNull(),
-  startDate: text("start_date").notNull(),
+  startDate: integer("start_date", { mode: "timestamp" }).notNull(),
   titleEN: text("title_en").notNull(),
   titleRU: text("title_ru").notNull(),
 })

6d090792a5ee2cb020ea80d877a2b2ba5d1c569e
Vasily Kuzin <exer7um@gmail.com>
2024-04-17 18:11:36 +0300

    Связь с тегами

--- server/database/schema/tag.ts
+++ server/database/schema/tag.ts
@@ -3,6 +3,7 @@ import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core"
 
 import { tagsToPersonalProjects } from "./tagsToPersonalProjects"
 import { tagsToProjects } from "./tagsToProjects"
+import { tagsToWorkplaces } from "./tagsToWorkplace"
 
 export const tags = sqliteTable("tags", {
   id: integer("id").primaryKey({ autoIncrement: true }),
@@ -14,4 +15,5 @@ export type Tag = typeof tags.$inferSelect
 export const tagsRelations = relations(tags, ({ many }) => ({
   personalProjects: many(tagsToPersonalProjects),
   projects: many(tagsToProjects),
+  workplaces: many(tagsToWorkplaces),
 }))

0b927ea3fedd50d5081c95c7e40f1e1771bddd2d
Vasily Kuzin <exer7um@gmail.com>
2024-04-18 18:49:12 +0300

    Компонент `WorkplacesCard`

---
+++ components/workplaces/card/WorkplacesCard.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import type { Tag } from "~/server/database/schema/tag"
+import type { Workplace } from "~/server/database/schema/workplace"
+
+defineProps<{
+  /** Проект */
+  workplace: {
+    description: string
+    tags: Tag[]
+    title: string
+  } & Omit<
+    Workplace,
+    "createdAt" | "descriptionEN" | "descriptionRU" | "titleEN" | "titleRU"
+  >
+}>()
+</script>
+
+<template>
+  <div
+    class="card col-span-full grid grid-flow-dense grid-cols-subgrid gap-y-6 p-5 sm:p-8"
+  >
+    <NuxtImg
+      :alt="workplace.title"
+      class="size-28 rounded-lg sm:size-40"
+      loading="lazy"
+      provider="cloudinary"
+      :src="workplace.logo"
+    />
+    <div class="col-span-2 flex flex-col gap-y-6 self-center lg:col-span-1">
+      <UiCardContent
+        :description="workplace.description"
+        large-title
+        :tags="workplace.tags"
+        :title="workplace.title"
+      />
+    </div>
+    <div
+      class="flex flex-col-reverse items-end gap-x-3 gap-y-2 self-start justify-self-end sm:flex-row sm:items-center"
+    >
+      <WorkplacesCardDate
+        data-test-id="startDate"
+        :date="workplace.startDate"
+      />
+      <hr
+        class="hidden w-6 border-neutral-200 sm:block dark:border-neutral-700"
+      />
+      <WorkplacesCardDate data-test-id="endDate" :date="workplace.endDate" />
+    </div>
+  </div>
+</template>
---
+++ tests/constants/workplaces.ts
@@ -0,0 +1,22 @@
+import { tags } from "~/tests/constants/tags"
+
+export const workplaces = [
+  {
+    description: "Описание места работы",
+    endDate: "2024-01-01T00:00:00.000Z",
+    id: 1,
+    logo: "/test-logo.webp",
+    startDate: "2023-01-01T00:00:00.000Z",
+    tags,
+    title: "Заголовок места работы",
+  },
+  {
+    description: "Описание места работы 2",
+    endDate: "2024-01-01T00:00:00.000Z",
+    id: 2,
+    logo: "/test-logo.webp",
+    startDate: "2023-01-01T00:00:00.000Z",
+    tags,
+    title: "Заголовок места работы 2",
+  },
+] as const

2b086dda251b1143f94fee6f10e649b30650bf7b
Vasily Kuzin <exer7um@gmail.com>
2024-04-27 19:30:41 +0300

    fix: Отступ при скролле к блокам (#463)
    Добавлен отступ при скролле к `UiBlock`.
    
    ### Связанная задача
    Fixes EXR7-137.

--- components/ui/UiBlock.vue
+++ components/ui/UiBlock.vue
@@ -6,7 +6,7 @@ defineProps<{
 </script>
 
 <template>
-  <section class="flex flex-col gap-y-6 sm:gap-y-8 lg:gap-y-10">
+  <section class="flex scroll-mt-32 flex-col gap-y-6 sm:gap-y-8 lg:gap-y-10">
     <h2 v-if="title">{{ title }}</h2>
     <slot />
   </section>

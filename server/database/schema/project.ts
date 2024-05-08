e65733934a3e201b11ab3272f979caccb54130fc
Vasily Kuzin <work@exer7um.com>
2024-05-08 15:15:08 +0300

    Использование общих полей

--- server/database/schema/project.ts
+++ server/database/schema/project.ts
@@ -1,21 +1,15 @@
-import { relations, sql } from "drizzle-orm"
+import { relations } from "drizzle-orm"
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core"
 
+import { baseCard } from "./common"
 import { tagsToProjects } from "./tagsToProjects"
 
 export const projects = sqliteTable("projects", {
   backgroundColor: text("background_color").notNull(),
-  createdAt: integer("created_at", { mode: "timestamp" })
-    .notNull()
-    .default(sql`(unixepoch())`),
-  descriptionEN: text("description_en").notNull(),
-  descriptionRU: text("description_ru").notNull(),
-  id: integer("id").primaryKey({ autoIncrement: true }),
   logo: text("logo").notNull(),
-  titleEN: text("title_en").notNull(),
-  titleRU: text("title_ru").notNull(),
   url: text("url").notNull(),
   year: integer("year").notNull(),
+  ...baseCard,
 })
 
 export type Project = typeof projects.$inferSelect
--- server/database/schema/tag.ts
+++ server/database/schema/tag.ts
@@ -1,12 +1,13 @@
 import { relations } from "drizzle-orm"
-import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core"
+import { sqliteTable, text } from "drizzle-orm/sqlite-core"
 
+import { id } from "./common"
 import { tagsToPersonalProjects } from "./tagsToPersonalProjects"
 import { tagsToProjects } from "./tagsToProjects"
 import { tagsToWorkplaces } from "./tagsToWorkplaces"
 
 export const tags = sqliteTable("tags", {
-  id: integer("id").primaryKey({ autoIncrement: true }),
+  ...id,
   title: text("title").notNull(),
 })
 

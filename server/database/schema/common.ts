793b0ab7da2e6691f91c62dd37ac5bb65cb4401c
Vasily Kuzin <exer7um@gmail.com>
2024-05-08 15:24:22 +0300

    refactor(api): Вынесение общих полей в отдельные переменные (#467)
    Вынесение общих полей в отдельные переменные в файле `common.ts`.
    
    ### Связанная задача
    EXR7-140.

---
+++ server/database/schema/common.ts
@@ -0,0 +1,61 @@
+import type { SQLiteColumnBuilderBase } from "drizzle-orm/sqlite-core"
+
+import { sql } from "drizzle-orm"
+import { integer, text } from "drizzle-orm/sqlite-core"
+
+/** Поля таблицы */
+type TableFields = Record<string, SQLiteColumnBuilderBase>
+
+/**
+ * Авто-увеличивающееся `ID` элемента.
+ *
+ * Добавляет поле `id`.
+ */
+export const id = {
+  id: integer("id").primaryKey({ autoIncrement: true }),
+} as const satisfies TableFields
+
+/**
+ * Локализованный заголовок.
+ *
+ * Добавляет поля `titleRU` и `titleEN`.
+ */
+export const localizedTitle = {
+  titleEN: text("title_en").notNull(),
+  titleRU: text("title_ru").notNull(),
+} as const satisfies TableFields
+
+/**
+ * Локализованное описание.
+ *
+ * Добавляет поля `descriptionRU` и `descriptionEN`.
+ */
+export const localizedDescription = {
+  descriptionEN: text("description_en").notNull(),
+  descriptionRU: text("description_ru").notNull(),
+} as const satisfies TableFields
+
+/**
+ * Дата создания.
+ *
+ * Добавляет поле `createdAt`.
+ */
+export const createdAt = {
+  createdAt: integer("created_at", { mode: "timestamp" })
+    .notNull()
+    .default(sql`(unixepoch())`),
+} as const satisfies TableFields
+
+/**
+ * Базовая карточка. Добавляет поля:
+ * - `ID`
+ * - Локализованный заголовок
+ * - Локализованное описание
+ * - Дата создания
+ */
+export const baseCard = {
+  ...createdAt,
+  ...localizedDescription,
+  ...localizedTitle,
+  ...id,
+} as const satisfies TableFields
--- server/database/schema/workplace.ts
+++ server/database/schema/workplace.ts
@@ -1,20 +1,14 @@
-import { relations, sql } from "drizzle-orm"
+import { relations } from "drizzle-orm"
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core"
 
+import { baseCard } from "./common"
 import { tagsToWorkplaces } from "./tagsToWorkplaces"
 
 export const workplaces = sqliteTable("workplaces", {
-  createdAt: integer("created_at", { mode: "timestamp" })
-    .notNull()
-    .default(sql`(unixepoch())`),
-  descriptionEN: text("description_en").notNull(),
-  descriptionRU: text("description_ru").notNull(),
   endDate: integer("end_date", { mode: "timestamp" }),
-  id: integer("id").primaryKey({ autoIncrement: true }),
+  ...baseCard,
   logo: text("logo").notNull(),
   startDate: integer("start_date", { mode: "timestamp" }).notNull(),
-  titleEN: text("title_en").notNull(),
-  titleRU: text("title_ru").notNull(),
 })
 
 export type Workplace = typeof workplaces.$inferSelect

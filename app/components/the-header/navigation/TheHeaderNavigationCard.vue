c6e02902730ebcd7cd09168be7313d93559a63d3
Vasily Kuzin <work@exer7um.com>
2024-07-09 17:54:18 +0300

    chore: Обновление зависимостей (#475)

--- app/components/the-header/navigation/TheHeaderNavigationCard.vue
+++ app/components/the-header/navigation/TheHeaderNavigationCard.vue
@@ -6,17 +6,21 @@ const props = defineProps<{
 
 const { t } = useI18n()
 const localeRoute = useLocaleRoute()
+
+/** Полученная ссылка навигации */
+const navigationRoute = computed(() =>
+  // @ts-expect-error - неправильно описан тип route
+  localeRoute({
+    name: props.routeName,
+  })
+)
 </script>
 
 <template>
   <NuxtLink
     class="relative flex justify-center leading-none transition-colors hover:text-blue-500"
     exact-active-class="cursor-default text-blue-600 after:absolute after:top-full after:mt-2 after:h-px after:w-6 after:bg-blue-600 hover:text-blue-600 dark:text-blue-500 after:dark:bg-blue-500 dark:hover:text-blue-500"
-    :to="
-      localeRoute({
-        name: toRefs(props).routeName.value,
-      })
-    "
+    :to="navigationRoute"
   >
     {{ t(`pages.${routeName}`) }}
   </NuxtLink>

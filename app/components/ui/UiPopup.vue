5d49642e2f2656b79dd270d7e99caa53432c9c21
Vasily Kuzin <work@exer7um.com>
2024-07-22 21:20:22 +0300

    fix: Добавление размытия фона для `UiPopup`

--- app/components/ui/UiPopup.vue
+++ app/components/ui/UiPopup.vue
@@ -48,7 +48,7 @@ const statusStyles = {
     open
     @close="emit('close')"
   >
-    <div class="fixed inset-0 bg-black/25" />
+    <div class="fixed inset-0 bg-black/15 backdrop-blur" />
     <HeadlessDialogPanel
       class="z-50 flex flex-col gap-y-6 rounded-2xl bg-white p-6 sm:p-8 dark:bg-neutral-900"
     >

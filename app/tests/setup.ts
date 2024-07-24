bbc3c72d2eba655ede9e137e26a6b54ec7d8befd
Vasily Kuzin <work@exer7um.com>
2024-07-24 18:03:46 +0300

    Обновление комментариев

--- app/tests/setup.ts
+++ app/tests/setup.ts
@@ -2,13 +2,11 @@ import { autoAnimatePlugin } from "@formkit/auto-animate/vue"
 import { config } from "@vue/test-utils"
 import { createI18n } from "vue-i18n"
 
-// НАСТРОЙКА I18N
-
 /** Объект с настройками i18n */
 const i18n = createI18n({
   locale: "ru", // По умолчанию выставляем русскую локализацию
   missing: (_, key) => key, // Если ключ не найден, возвращаем сам ключ
 })
 
-// Добавляем i18n в плагины
+// Добавляем плагины
 config.global.plugins.push(i18n, autoAnimatePlugin)

fc24f06c90971a571bc67baf67c3e055e4baccc4
Vasily Kuzin <work@exer7um.com>
2024-10-11 18:23:45 +0300

    fix: Адаптив для подзаголовка `UiBlock`

--- app/components/ui/UiBlock.vue
+++ app/components/ui/UiBlock.vue
@@ -11,7 +11,10 @@ defineProps<{
 
 <template>
   <section class="flex scroll-mt-32 flex-col gap-y-6 sm:gap-y-8 lg:gap-y-10">
-    <div v-if="!hideTitle" class="flex items-baseline gap-x-4">
+    <div
+      v-if="!hideTitle"
+      class="flex flex-col gap-y-1 sm:flex-row sm:items-baseline sm:gap-x-4"
+    >
       <h2>{{ title }}</h2>
       <h3 v-if="subTitle" class="text-neutral-400 dark:text-neutral-600">
         {{ subTitle }}

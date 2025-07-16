1621dbfa67a6f5a1eb94a05a9cd4ea0f5c9ffeaa
Vasily Kuzin <work@exer7um.com>
2025-07-16 14:22:01 +0300

    Обновление импортов Zod

--- server/api/messages/index.post.ts
+++ server/api/messages/index.post.ts
@@ -1,4 +1,4 @@
-import { z } from "zod/v4-mini"
+import { z } from "zod/mini"
 
 /** Сообщение пользователя */
 const messageSchema = z.object({
--- server/utils/zod.ts
+++ server/utils/zod.ts
@@ -1,4 +1,4 @@
-import { z } from "zod/v4-mini"
+import { z } from "zod/mini"
 
 /** Лимит количества результатов */
 export const limit = z.optional(z.coerce.number().check(z.int()))

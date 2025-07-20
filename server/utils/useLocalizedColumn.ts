6c3fcac00db0101a8bafa116776b9bd962f93935
Vasily Kuzin <work@exer7um.com>
2025-07-20 15:55:00 +0300

    Исправление ошибки типизации

--- server/utils/useLocalizedColumn.ts
+++ server/utils/useLocalizedColumn.ts
@@ -11,6 +11,7 @@ import { getHeaderLocale } from "@intlify/h3"
  * @returns локализированное название колонки
  */
 export default function <TReturn>(column: string, event: H3Event): TReturn {
+  // @ts-expect-error: Берется неправильный тип, но правильная функция
   const locale = getHeaderLocale(event, { lang: "ru" })
 
   return (column + locale.baseName.toUpperCase()) as TReturn

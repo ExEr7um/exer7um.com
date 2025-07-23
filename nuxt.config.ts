88d4fe6b4673ab3fcdf8d0c8e72325e4881827fd
Vasily Kuzin <work@exer7um.com>
2025-07-23 16:46:39 +0300

    chore(deps): Обновление зависимостей (#514)
    Обновление зависимостей и включение `rolldown-vite`

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -47,6 +47,7 @@ export default defineNuxtConfig({
   image: {
     cloudinary: { baseURL: "https://res.cloudinary.com/exer7um/image/upload/" },
   },
+  linkChecker: { enabled: false },
   nitro: { preset: "cloudflare-pages" },
   ogImage: { zeroRuntime: true },
   robots: { blockNonSeoBots: true },

ba44d6723cba0acb789c3fb2637fcf8e7a1a902a
Vasily Kuzin <work@exer7um.com>
2025-10-24 13:19:29 +0300

    Обновление тестов

--- app/tests/components/the-header/TheHeader.spec.ts
+++ app/tests/components/the-header/TheHeader.spec.ts
@@ -8,6 +8,11 @@ import TheHeader from "~/components/the-header/TheHeader.vue"
 describe("Компонент TheHeader", () => {
   let wrapper: VueWrapper
 
+  const desktopNavigation = () =>
+    wrapper.findComponent({ name: "UNavigationMenu" })
+  const mobileNavigation = () =>
+    wrapper.findComponent({ name: "TheHeaderNavigationMobile" })
+
   beforeEach(async () => {
     wrapper = await mountSuspended(TheHeader, { shallow: true })
   })
@@ -18,5 +23,33 @@ describe("Компонент TheHeader", () => {
 
   test("Семантический тег header", () => {
     expect(wrapper.element.tagName).toBe("HEADER")
+  })
+
+  describe("Десктопная навигация", () => {
+    test("Отображается", () => {
+      expect(desktopNavigation().exists()).toBeTruthy()
+    })
+
+    test("Передаются элементы навигации", () => {
+      expect(desktopNavigation().props("items")).toMatchSnapshot()
+    })
+
+    test("Отображается на десктопных устройствах", () => {
+      expect(desktopNavigation().classes()).toContain("md:flex")
+    })
+  })
+
+  describe("Мобильная навигация", () => {
+    test("Отображается", () => {
+      expect(mobileNavigation().exists()).toBeTruthy()
+    })
+
+    test("Передаются элементы навигации", () => {
+      expect(mobileNavigation().props("navigation")).toMatchSnapshot()
+    })
+
+    test("Скрывается на десктопных устройствах", () => {
+      expect(mobileNavigation().classes()).toContain("md:hidden")
+    })
   })
 })

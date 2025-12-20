10fc6c1a08dd4f22f807bad1050b3954e0ecce7a
Vasily Kuzin <work@exer7um.com>
2025-12-20 13:15:33 +0300

    Обновление обращения к Drizzle

--- server/api/personal-projects/index.get.ts
+++ server/api/personal-projects/index.get.ts
@@ -1,25 +1,23 @@
-import { sql } from "drizzle-orm"
-
 export default defineEventHandler(async (event) => {
   const { limit } = await getValidatedQuery(event, (query) =>
     limitQuerySchema.parse(query),
   )
 
   /** Список проектов */
-  const personalProjects = await useDrizzle().query.personalProjects.findMany({
+  const personalProjects = await db.query.personalProjects.findMany({
     columns: { github: true, icon: true, id: true },
     extras: {
       description:
-        sql<string>`${tables.personalProjects[useLocalizedColumn<"descriptionEN" | "descriptionRU">("description", event)]}`.as(
+        sql<string>`${schema.personalProjects[useLocalizedColumn<"descriptionEN" | "descriptionRU">("description", event)]}`.as(
           "description",
         ),
       title:
-        sql<string>`${tables.personalProjects[useLocalizedColumn<"titleEN" | "titleRU">("title", event)]}`.as(
+        sql<string>`${schema.personalProjects[useLocalizedColumn<"titleEN" | "titleRU">("title", event)]}`.as(
           "title",
         ),
     },
     limit: limit, // Ограничиваем количество результатов
-    orderBy: desc(tables.personalProjects.createdAt), // Сортируем по дате создания — сначала новые
+    orderBy: desc(schema.personalProjects.createdAt), // Сортируем по дате создания — сначала новые
     with: { tags: { columns: {}, with: { tag: true } } },
   })
 
